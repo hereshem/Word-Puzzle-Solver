19915f3c775fb9ff8a56cf2c27c0c5ffb6e41e4b
Hem Shrestha <hereshem@gmail.com>
2019-03-08 01:52:32 +0545

    ready to publish

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Word Puzzle Solver
+
+Word Puzzle Solver is designed to solve your dictionary words related problem. Install any word puzzle related app and try to solve the problem in much easier way then ever was.
+
+![](ScreenShot_2019-03-07.png)
---
+++ words/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+w = []
+def get_words_from_file():
+    if w: return w
+    with open('words/words.txt') as fin:
+        for l in fin: w.append(l.strip())
+    print("reading file with words", len(w))
+    return w
+
+def check_letter(x,y):
+    c = list(y)
+    for i in x:
+        if i in c: c.remove(i)
+        else: return False
+    return True
+
+def find_words(number, chars):
+    return list(filter(lambda x: len(x)==number and check_letter(x, chars), get_words_from_file()))
+
+def home(request):
+    data = {}
+    if request.method == "POST":
+        number = request.POST.get("number")
+        chars = request.POST.get("chars")
+        data["number"] = number
+        data["chars"] = chars
+        if number and chars and number.isdigit():
+            data["words"] = find_words(int(number), chars.lower())
+    return render(request, "words/index.html", data)
